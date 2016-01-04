d57b349ae1276e0135321186fd6399dd64202d85
Andreas Amundsen <andreas.amundsen123@gmail.com>
2016-01-04 18:22:55 +0100

    Changed base_url and added 'get_lended_books' in actions

--- src/backend.py
+++ src/backend.py
@@ -1,12 +1,13 @@
 import requests
 
-base_url = "http://www.tryggestad.com/liblog/"
+base_url = "http://norbye.com/-other-/liblog/"
 actions = {
     'create_user': 'create_user.php',
     'get_user_info': 'get_user_info.php',
     'delete_user': 'delete_user.php',
     'lend_book': 'lend_book.php',
     'deliver_book': 'deliver_book.php',
+    'get_lended_books': 'get_lended_books.php',
 }
 
 def request(action, data):
